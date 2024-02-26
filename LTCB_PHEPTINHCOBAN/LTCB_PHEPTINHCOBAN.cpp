bdd86ac1f28fd9fefd54d120a980a4fcd1aab215
ADMIN <ADMIN@LAB1-MAY07>
2024-02-26 10:30:01 +0700

    Add project files.

---
+++ LTCB_PHEPTINHCOBAN/LTCB_PHEPTINHCOBAN.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+
+
+int tinhtong(int so1, int so2) {
+	//int sotong = so1+so2;
+	//return sotong;
+
+	return so1 + so2;
+}
+int main() {
+	//khai báo biến
+	int so1, so2, tong;
+
+	//nhap dữ liệu vao biến
+	cout << "chuong trinh tinh tong 2 so\n";
+	cout << "nhap so thu nhat:";
+	cin >> so1;
+	cout << "nhap so thu hai:";
+	cin >> so2;
+
+	//tính toan
+	tong = tinhtong(so1, so2);
+
+	//xuất dữ liệu
+	cout << so1 << "+" << so2 << "=" << tong << endl;
+	return 0;
+}
